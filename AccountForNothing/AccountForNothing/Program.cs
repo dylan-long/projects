37b02a5b92bb4e5aa06d95b846ffb8bc70cf64fa
dyaln long <dylan.long1898@gmail.com>
2018-06-05 20:31:51 -0700

    NEW C# AND ASP.NET COURSE pg.23
    account THIS.

---
+++ AccountForNothing/AccountForNothing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace AccountForNothing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to UNP accounting");
+            Console.WriteLine("where we account for nothing!");
+            Console.WriteLine("P.s. please help us we have a terrable buissnes model.");
+            Console.ReadLine();
+                }
+    }
+}
