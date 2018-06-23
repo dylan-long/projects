255db22bc0f6d518f6755d93bc501bc8ca1fd818
dyaln long <dylan.long1898@gmail.com>
2018-06-23 15:31:20 -0700

    c# page 60 method exercise

---
+++ methodPractice/methodPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodPractice
+{
+   public class Program
+    {
+        
+        public static void Main(string[] arg)
+        {
+           
+
+            Console.WriteLine("please input a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("this is your number: " + userInput);
+            int product = MathOperations.Multiply(userInput);
+            int sum = MathOperations.Add(userInput);
+            int quotient = MathOperations.Divide(userInput);
+
+            Console.WriteLine(product + " this is your number times 4");
+            Console.WriteLine(sum + " this is your number plus 10");
+            Console.WriteLine(quotient + " this is your number divided by 2 (rounded to the nearest whole number)");
+
+            Console.ReadLine();
+
+        }
+    }
+   
+}
+
---
+++ methodPractice/methodPractice/methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodPractice
+{
+    public class MathOperations
+    {
+
+        public static int Multiply(int num1)
+        {
+            num1 = num1 * 4;
+            return num1;
+
+        }
+
+        public static int Add(int num2)
+        {
+            num2 = num2 + 10;
+            return num2;
+        }
+
+        public static int Divide(int num3)
+        {
+            num3 = num3 / 2;
+            return num3;
+        }
+
+    }
+
+}
