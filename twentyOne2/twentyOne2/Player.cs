76024a86025dff6c6eebf17ae7ae4d3022a98026
dyaln long <dylan.long1898@gmail.com>
2018-06-29 19:59:54 -0700

    TwentyOneGame
    line 28 in twentyOneGame.cs sets the boolean value as false everytime not sure why. the methods i used to instantiate a new list is different than the method used in the video (see Game.cs properties) i wonder if thats the issue, however when i try the same method as the video it whites out the private property and tells me to do it that way. (video on page 100 c#)

---
+++ twentyOne2/twentyOne2/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyOne2
+{
+    public class Player
+    {   public Player(string name, int beginningBalance)
+        {
+            Hand = new List<Card>();
+            Balance = beginningBalance;
+            Name = name;
+        }
+        public List<Card> Hand { get; set; } = new List<Card>();
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public bool isActivelyPlaying { get; set; }
+        public bool Stay { get; set; }
+
+        public bool Bet (int amount)
+        {
+            if (Balance - amount < 0)
+            {
+                Console.WriteLine("you dont have enough money to bet that much");
+                return false;
+            }
+            else
+            {
+                Balance -= amount;
+                return true;
+            }
+        }
+
+        public static Game operator + (Game game, Player player)
+        {
+            game.Players.Add(player);
+            return game;
+        }
+        public static Game operator -(Game game, Player player)
+        {
+            game.Players.Remove(player);
+            return game;
+        }
+    }
+}
---
+++ twentyOne2/twentyOne2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twentyOne2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the game! whats ya name.");
+            String playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring to the table");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes")
+            {
+                Player player = new Player(playerName, bank );
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thanks for playin!");
+            }
+            Console.WriteLine("bye for now!");
+            Console.Read();
+        }
+    
+    }
+}
